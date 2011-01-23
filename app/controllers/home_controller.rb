86df088aa7022ec32ac17d10eaf86925bc4c8622
vinhboy <vinhboy@gmail.com>
2011-01-23 14:32:03 -0800

    javascript working

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,43 +2,39 @@ class HomeController < ApplicationController
   def index
     if params[:commands]
       @commands = params[:commands].delete!("\r").split(/\n/).collect! do |command|
-        command.split(/ /)
+        command.upcase.split(/ /)
       end
       grid = @commands[0].collect(&:to_i)
-      @rover_one = move(grid,@commands[1],@commands[2])
-      @rover_two = move(grid,@commands[3],@commands[4])
+      @rover_one = move(grid,Array.new(@commands[1]),@commands[2]) if @commands[1] and @commands[2]
+      @rover_two = move(grid,Array.new(@commands[3]),@commands[4]) if @commands[3] and @commands[4]
     end
   end
   
-  def move(grid,start,directions)
-    directions.each do |direction|
-      start[2] = turn(start,direction) if direction == 'L' or direction == 'R'
-      
-      if direction == 'M'
-        if start[2] == 'N' or start[2] == 'S'
-          dir = start[2] == 'N' ? 1 : -1
-          if (start[1].to_i + dir) <= grid[1]
-            start[1] = start[1].to_i + dir
-          end
-        end
-        if start[2] == 'E' or start[2] == 'W'
-          dir = start[2] == 'E' ? 1 : -1
-          if (start[0].to_i + dir) <= grid[0]
-            start[0] = start[0].to_i + dir
-          end
-        end
-      end
+  def move(grid,start,commands)
+    commands.each do |command|
+      turn(start,command) if command == 'L' or command == 'R'
+      forward(grid,start) if command == 'M'
     end
     start
   end
   
-  def turn(start,direction)
+  def turn(start,command)
     compass = Hash['N'=>['W','E'],'E'=>['N','S'],'S'=>['E','W'],'W'=>['S','N']]
-    compass[start[2]][direction == 'L' ? 0 : 1]
-    # if (oldpos = compass.index(start[2]))
-    #   dir = direction == 'R' ? 1 : -1
-    #   oldpos = -1 if (oldpos > 2)
-    #   compass[oldpos + dir]
-    # end
+    start[2] = compass[start[2]][command == 'L' ? 0 : 1]
+  end
+  
+  def forward(grid,start)
+    if start[2] == 'N' or start[2] == 'S'
+      dir = start[2] == 'N' ? 1 : -1
+      if (0..grid[1]) === (start[1].to_i + dir)
+        start[1] = start[1].to_i + dir
+      end
+    end
+    if start[2] == 'E' or start[2] == 'W'
+      dir = start[2] == 'E' ? 1 : -1
+      if (0..grid[0]) === (start[0].to_i + dir)
+        start[0] = start[0].to_i + dir
+      end
+    end
   end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,62 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function() {
+  $('form').submit(function(){
+    var commands = new Array();
+    $.each($('textarea').val().split(/\n/),function(index,value){
+      commands.push(value.toUpperCase().split(" "));
+    });
+    $('#right').html('<ul></ul>');
+    var i = 0;
+    for (i=0;i<=commands[0][0];i++) {
+      $('#right ul').append('<li></li>');
+    }
+    for (i=1;i<=commands[0][1];i++) {
+      $('#right').append($('#right ul:first').clone());
+    }
+    var robot = '';
+    var grid = commands[0];
+    if (commands[1] && commands[2]) {
+      robot = $('#rover_one').clone().show().appendTo($('ul:nth-child(' + (parseInt(grid[1],10) + 1 - parseInt(commands[1][1],10)) + ') li:nth-child('+(parseInt(commands[1][0],10)+1)+')')).fadeIn('slow').data('position',commands[1]);
+      move(grid,commands[2],robot);
+    }
+    if (commands[3] && commands[4]) {
+      robot = $('#rover_two').clone().show().appendTo($('ul:nth-child(' + (parseInt(grid[1],10) + 1 - parseInt(commands[3][1],10)) + ') li:nth-child('+(parseInt(commands[3][0],10)+1)+')')).fadeIn('slow').data('position',commands[3]);
+      move(grid,commands[4],robot);
+    }
+    return false;
+  });
+  
+  function move(grid,commands,robot) {
+    $.each(commands,function(index,command){
+      if (command == 'L' || command == 'R') turn(command,robot);
+      if (command == 'M') go_forward(grid,robot);
+    });
+    console.log(robot);
+    console.log(robot.data('position'));
+  }
+  
+  function turn(dir,robot) {
+    compass = {'N':['W','E'],'E':['N','S'],'S':['E','W'],'W':['S','N']};
+    var position = robot.data('position');
+    position[2] = compass[position[2]][dir == 'L' ? 0 : 1];
+    robot.data('position',position);
+  }
+  
+  function go_forward(grid,robot) {
+    var position = robot.data('position');
+    if (position[2] == 'N' || position[2] == 'S') {
+      var y = parseInt(position[1],10) + (position[2] == 'N' ? 1 : -1);
+      if (y >= 0 && y <= grid[1]) {
+        position[2] == 'N' ? robot.animate({top: '-=91'}, 2000) : robot.animate({top: '+=91'}, 2000);
+        position[1] = y;
+      }
+    }
+    if (position[2] == 'E' || position[2] == 'W') {
+      var x = parseInt(position[0],10) + (position[2] == 'E' ? 1 : -1);
+      if (x >= 0 && x <= grid[0]) {
+        position[2] == 'E' ? robot.animate({left: '+=98'}, 2000) : robot.animate({left: '-=98'}, 2000);
+        position[0] = x;
+      }
+    }
+    robot.data('position',position);
+  }
+});
